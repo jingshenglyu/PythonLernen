6988bdfb88a47ee045a5d04f9731d29d4b7c719a
Jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2018-12-08 16:47:12 +0100

    This is object oriented.

---
+++ object oriented.py
@@ -0,0 +1,17 @@
+# object oriented
+
+class Student():
+    def eat(self):  # 3 and 4 Zeile are method.
+        print("Jason can eat")
+
+    def study(self):
+        print("Jason can study")
+
+
+# Student().eat()
+#
+# fruit ="apple"
+# print(fruit.upper()) # upper can change capital letter.
+
+Jason = Student()
+Jason.study()

70ab31393b3b0a313437930442dfa551b4d870f7
Jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2018-12-08 15:24:52 +0100

    This is a function syntax.

---
+++ func practise.py
@@ -0,0 +1,37 @@
+# function learn
+
+# def function_name()
+# # define a function
+
+# def func1():
+#     print("hello function")
+#
+# func1()
+#
+# a = 10
+# b = 10
+#
+# print(a+b)
+#
+# c = 20
+# d = 20
+# print(c+d)
+
+# def sum(num1,num2):
+#     print(num1+num2)
+#
+# sum(1,2)
+# sum(10,20)
+
+def fun2():
+    return "hello func2" # fanhui zhi
+
+def sum2(a,b):
+    return(a+b)
+
+b = sum2(1,1)
+print(b)
+
+
+a = fun2()
+print(a)

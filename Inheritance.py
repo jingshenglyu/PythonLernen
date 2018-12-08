45645ed318e8e9d18ec8b194df545a349609b0b2
Jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2018-12-08 20:37:58 +0100

    This is inheritance.

---
+++ Inheritance.py
@@ -0,0 +1,19 @@
+# inheritance yichan
+
+class Father:
+    name = "Jason"
+    def eat(self):
+        print("I can eat")
+
+class Mother:
+    def walk(self):
+        print('walk like mother')
+
+class Son(Father,Mother):  # inheritance class Father/Mother
+    def eat(self):
+        print("eat like son") # overriden
+    def walk(self):
+        print("walk like son")
+
+littleJason = Son()
+littleJason.walk()

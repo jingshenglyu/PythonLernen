cabbac906b28a676d7e042c3fb51102d7e90869d
Jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2018-12-08 15:01:42 +0100

    This is a if/else if/else.

---
+++ for loop.py
@@ -0,0 +1,18 @@
+# for
+
+# Syntax for i in XXX:
+
+my_list=[1,2,3,4,5,6,7,8,9,10]
+
+
+# print(my_list[0])
+# print(my_list[1])
+#
+# for number in my_list:
+#     print(number)
+
+for num in range(10,21):
+    print(num+1)
+
+for letter in "Jason":
+    print("each letter is:"+letter)

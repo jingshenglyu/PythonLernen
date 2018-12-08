edb835ea2d339d3476f91ba7e9129b7272e19ea7
Jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2018-12-08 15:13:13 +0100

    This is a while loop.

---
+++ while loop.py
@@ -0,0 +1,17 @@
+# while loop
+
+condition = 1
+
+# while condition<10:
+#     print(condition)
+#     condition = condition + 1
+#     # break # stop command
+#
+# for i in range(5):
+#     print(i)
+
+while True:
+    a = int(input("please enter a number a:"))
+    b = int(input("please enter a number b:"))
+    print("a+b="+str(a+b))
+    break

ac0d1ac4e338cebbe23ad6c84e7780b1fd5b1c5b
Jingsheng <jingsheng.lyu@campus.tu-berlin.de>
2018-12-08 16:09:01 +0100

    This is a function syntax.

---
+++ convertor.py
@@ -0,0 +1,20 @@
+# huansuanqi
+
+# def converter(argument)
+
+def converter(weight):
+    ponds = weight/0.45
+    print(ponds)
+
+converter(60)
+
+converter(100)
+
+converter(200)
+
+def converter2(weight=100):
+    ponds = weight / 0.45
+    print(ponds)
+# default is 100 ponds.
+
+converter2()
